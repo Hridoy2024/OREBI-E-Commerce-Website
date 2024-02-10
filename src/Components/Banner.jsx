d48147cc9611aaf5dad224ac236c01a649e35d65
hridoy2024 <hridu2024@gmail.com>
2024-02-10 21:25:29 +0600

    last update

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,33 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "../Components/Image";
 import BannerImage from "../assets/banner.png";
 import Container from "./Container";
 import Flex from "./Flex";
 import { FaTruck } from "react-icons/fa";
 import { VscDebugRestart } from "react-icons/vsc";
+import "slick-carousel/slick/slick.css";
+import Slider from "react-slick";
 
 const Banner = () => {
+  const [slide, setSlide] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    beforeChange: (prev, next) => {
+      setSlide(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "10px",
+          transform: "translateY(-50%)",
+          padding: "10px",
+        }}
+      >
+        <ul style={{ margin: "0px" }}> {dots} </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={
+          i == slide
+            ? {
+                padding: "5px 0",
+                width: "30px",
+                color: "#262626",
+                borderRight: "2px #262626 solid",
+              }
+            : {
+                padding: "5px 0",
+                width: "30px",
+                color: "white",
+                borderRight: "2px white solid",
+              }
+        }
+      >
+        0{i + 1}
+      </div>
+    ),
+
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <section>
-      <Image src={BannerImage} />
+      <Slider {...settings}>
+        <Image src={BannerImage} />
+        <Image src={BannerImage} />
+        <Image src={BannerImage} />
+        <Image src={BannerImage} />
+      </Slider>
       <div className="bg-white py-[30px]">
         <Container>
-          <Flex className={`justify-between`}>
-            <Flex className={`items-center gap-[17px]`}>
+          <Flex className={`justify-between flex-col md:flex-row `}>
+            <Flex className={`items-center gap-[17px] justify-center my-3 md:my-0`}>
               <span className="font-dm font-bold text-[30px]">2</span>
               <p className="font-dm font-normal text-[16px] text-[#6D6D6D]">
                 Two years warranty
               </p>
             </Flex>
 
-            <Flex className={`items-center gap-[15px]`}>
+            <Flex className={`items-center gap-[15px] justify-center my-3 md:my-0`}>
               <FaTruck size={25} />
               <p className="font-dm font-normal text-[16px] text-[#6D6D6D]">
                 Free shipping
               </p>
             </Flex>
 
-            <Flex className={`items-center gap-[15px]`}>
+            <Flex className={`items-center gap-[15px] justify-center my-3 md:my-0`}>
               <VscDebugRestart size={25} />
               <p className="font-dm font-normal text-[16px] text-[#6D6D6D]">
                 Return policy in 30 days
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,12 +8,17 @@ import { FaSearch } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { FaShoppingCart } from "react-icons/fa";
+import Image from "./Image";
+import img from "../assets/headerimg.png";
+import { FaTimes } from "react-icons/fa";
 
 const Header = () => {
   const dropDownRef = useRef();
   const accountRef = useRef();
+  const cartRef = useRef();
   const [dropDown, setDropDown] = useState(false);
   const [account, setAccount] = useState(false);
+  const [cartDrop, setCartDrop] = useState(false);
 
   useEffect(() => {
     document.addEventListener("click", (e) => {
@@ -31,23 +36,27 @@ const Header = () => {
       accountRef.current.contains(e.target)
         ? setAccount(true)
         : setAccount(false);
+
+      cartRef.current.contains(e.target)
+        ? setCartDrop(true)
+        : setCartDrop(false);
     });
   }, []);
   return (
     <header className="bg-[#F5F5F3] py-[25px]">
       <Container>
-        <Flex className={`justify-between items-center `}>
+        <Flex className={`justify-between items-center gap-3 lg:gap-0`}>
           {/* catagory button */}
           <div ref={dropDownRef} className="relative">
             <Flex className={`items-center gap-[10px] `}>
               <FaBarsProgress />
-              <span className="font-dm font-normal text-[14px] text-[#262626]">
+              <span className="font-dm font-normal text-[14px] text-[#262626] hidden lg:block">
                 Shop by Category
               </span>
             </Flex>
 
             {dropDown && (
-              <Listul className={`bg-[#262626] w-[263px] mt-[15px absolute`}>
+              <Listul className={`bg-[#262626] w-[263px] mt-[15px absolute z-50`}>
                 <Listli
                   className={`py-4 pl-5 block font-dm font-normal text-[14px] text-white opacity-50 hover:font-bold hover:opacity-100 hover:pl-[30px] duration-300 ease-in-out `}
                 >
@@ -85,9 +94,9 @@ const Header = () => {
           {/* catagory button end */}
 
           {/* Search  */}
-          <div className="relative">
+          <div className="relative w-[600px]">
             <input
-              className="w-[600px] py-4 pl-5 placeholder:font-dm font-normal text-[14px] text-[#c4c4c4] "
+              className="w-full py-4 pl-5 placeholder:font-dm font-normal text-[14px] text-[#c4c4c4] "
               type="text"
               placeholder="Search Products"
             />
@@ -104,22 +113,64 @@ const Header = () => {
                 <IoMdArrowDropdown className="text-bold" />
               </Flex>
 
-              {account &&  <Listul className={`mt-[15px] absolute right-0 w-[200px]`}>
-                <Listli
-                  className={` py-4 px-[58px] text-center  block font-dm font-bold text-[14px] text-white bg-primary `}
-                >
-                  My Account
-                </Listli>
+              {account && (
+                <Listul className={`mt-[15px] absolute right-0 w-[200px] z-50`}>
+                  <Listli
+                    className={` py-4 px-[58px] text-center  block font-dm font-bold text-[14px] text-white bg-primary `}
+                  >
+                    My Account
+                  </Listli>
 
-                <Listli
-                  className={` py-4 px-[58px]  text-center block font-dm font-normal text-[14px] text-primary bg-white `}
-                >
-                  Log Out
-                </Listli>
-              </Listul>}
+                  <Listli
+                    className={` py-4 px-[58px]  text-center block font-dm font-normal text-[14px] text-primary bg-white `}
+                  >
+                    Log Out
+                  </Listli>
+                </Listul>
+              )}
             </div>
 
-            <FaShoppingCart className="" />
+            <div ref={cartRef} className="relative">
+              <FaShoppingCart className="text-primary cursor-pointer" />
+
+              {cartDrop && (
+                <div className="w-[360px] bg-white absolute right-0 shadow-sm z-50">
+                  <Flex
+                    className={`p-5 bg-[#f5f5f3] items-center gap-5 relative`}
+                  >
+                    <Image src={img} />
+
+                    <div>
+                      <h3 className="font-dm font-bold text-[14px] text-primary  ">
+                        Black Smart Watch
+                      </h3>
+
+                      <p className="font-dm font-bold text-[14px] text-primary  ">
+                        $44.00
+                      </p>
+                    </div>
+
+                    <FaTimes className="absolute top-[50%] translate-y-[-50%] right-3" />
+                  </Flex>
+
+                  <div className="p-5">
+                    <p className="font-dm font-normal text-[16px] leading-[23px] text-secondary  ">
+                      Subtotal:{" "}
+                      <span className="font-bold text-primary ">$44.00</span>
+                    </p>
+
+                    <Flex className={`justify-between mt-3 `}>
+                      <button className=" py-4 px-10 border-[1px] border-[#2b2b2b] font-dm font-bold text-[14px] text-primary  ">
+                        View Cart
+                      </button>
+                      <button className=" py-4 px-10 border-[1px] border-[#2b2b2b] font-dm font-bold text-[14px] text-white bg-primary  ">
+                        Checkout
+                      </button>
+                    </Flex>
+                  </div>
+                </div>
+              )}
+            </div>
           </Flex>
           {/* Login End*/}
         </Flex>
